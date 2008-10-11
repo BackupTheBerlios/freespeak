334399b6169494e1749996b65ccbfe813059cec6
lethalman <lethalman@aecf430d-c2fa-0310-b08f-8e4b508fadd5>
2008-10-11 12:57:56 +0000

    ui.ScrolledWindow created to replace make_text
    git-svn-id: svn+ssh://svn.gna.org/svn/freespeak/trunk@27 aecf430d-c2fa-0310-b08f-8e4b508fadd5

--- freespeak/ui/exception_dialog.py
+++ freespeak/ui/exception_dialog.py
@@ -1,25 +1,26 @@
 import traceback
 
+from freespeak import ui
+
 class ExceptionDialog (gtk.Dialog):
     """
     Show non-catched exception in a window
     """
     def __init__(self, *tb):
-        gtk.Dialog.__init__(self, 'FreeSpeak - '+_('Exception'), None, 0,
-                            (gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
+        gtk.Dialog.__init__ (self, 'FreeSpeak - '+_('Exception'), None, 0,
+                             (gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
         self.set_keep_above (1)
         self.set_resizable (1)
         self.set_default_size (400, 300)
         
         label = gtk.Label()
-        label.set_markup('<span color="red"><b>'+
-                         _('An error was occurred:')+'</b></span>')
+        label.set_markup ('<span color="red"><b>'+_('An error has occurred:')+'</b></span>')
         label.show()
-        self.vbox.pack_start(label, 0, 0)
+        self.vbox.pack_start (label, 0, 0)
         text = gtk.TextView()
-        text.get_buffer().set_text(''.join(traceback.format_exception (*tb)))
-        scroll = make_text (text)
-        scroll.show()
-        self.vbox.pack_start(scroll)
+        text.get_buffer().set_text (''.join (traceback.format_exception (*tb)))
+        scroll = ui.ScrolledWindow (text)
+        scroll.show ()
+        self.vbox.pack_start (scroll)
         
         self.run ()
