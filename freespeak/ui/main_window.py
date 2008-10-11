0517a4e54e22d02d5bd23537de87fa8f1a38f672
lethalman <lethalman@aecf430d-c2fa-0310-b08f-8e4b508fadd5>
2008-10-11 13:00:32 +0000

    Moved util dialogs to ui.utils
    git-svn-id: svn+ssh://svn.gna.org/svn/freespeak/trunk@46 aecf430d-c2fa-0310-b08f-8e4b508fadd5

--- freespeak/ui/main_window.py
+++ freespeak/ui/main_window.py
@@ -95,35 +95,6 @@ class MainWindow (gtk.Window):
 #                 self.queue = Queue()
 #                 thread.start_new_thread(self.ipc_client, ())
 #                 thread.start_new_thread(self.ipc_server, ())
-            
-        def error(self, message, parent=None):
-            """
-            Run an message dialog for an error
-            """
-            if not parent: parent = self
-            dialog = gtk.MessageDialog(parent,
-                                       gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                                       gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, message)
-            dialog.run()
-            dialog.destroy()
-            
-            def question(self, msg, parent=None):
-                """
-                Ask a question and return the response of the message dialog
-                @param msg: The message to be shown
-                @param parent: Specify the transient parent
-                @return: Dialog response
-                """
-                if not parent: parent = self
-                dialog = gtk.MessageDialog(parent,
-                                           gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                                           gtk.MESSAGE_QUESTION, gtk.BUTTONS_NONE, msg)
-                dialog.add_buttons(gtk.STOCK_YES, gtk.RESPONSE_YES,
-                                   gtk.STOCK_NO, gtk.RESPONSE_NO) 
-                dialog.set_default_response(gtk.RESPONSE_NO)
-                response = dialog.run()
-                dialog.destroy()
-                return response
         
             def load_modules(self):
                 """
--- freespeak/ui/utils.py
+++ freespeak/ui/utils.py
@@ -4,3 +4,30 @@ class ScrolledWindow (gtk.ScrolledWindow):
     def __init__ (self, child):
         gtk.ScrolledWindow.__init__ (self)
         self.add (child)
+
+def error (message, parent=None):
+    """
+    Run an message dialog for an error
+    """
+    dialog = gtk.MessageDialog(parent,
+                               gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                               gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, message)
+    dialog.run ()
+    dialog.destroy()
+            
+def question(self, message, parent=None):
+    """
+    Ask a question and return the response of the message dialog
+    @param msg: The message to be shown
+    @param parent: Specify the transient parent
+    @return: Dialog response
+    """
+    dialog = gtk.MessageDialog(parent,
+                               gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                               gtk.MESSAGE_QUESTION, gtk.BUTTONS_NONE, msg)
+    dialog.add_buttons(gtk.STOCK_YES, gtk.RESPONSE_YES,
+                       gtk.STOCK_NO, gtk.RESPONSE_NO) 
+    dialog.set_default_response(gtk.RESPONSE_NO)
+    response = dialog.run()
+    dialog.destroy()
+    return response
