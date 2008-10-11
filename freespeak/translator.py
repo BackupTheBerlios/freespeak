c4e91fb7c4d3d29a3e163414c1ca50cc315df18d
lethalman <lethalman@aecf430d-c2fa-0310-b08f-8e4b508fadd5>
2008-10-11 13:03:20 +0000

    Module loading organization in TranslatorsManager
    git-svn-id: svn+ssh://svn.gna.org/svn/freespeak/trunk@67 aecf430d-c2fa-0310-b08f-8e4b508fadd5

--- freespeak/translator.py
+++ freespeak/translator.py
@@ -10,11 +10,19 @@ class TranslatorsManager (list):
 
         files = glob.glob (os.path.join (self.application.translators_path, "*.py"))
         for fname in files:
-            # 1. Split the path and get the latest part of it
-            # 2. Split the extension and get the name without .py
-            mname = os.path.splitext(os.path.split(fname)[1])[0]
-            if mname == '__init__':
+            module = self.load_translator_from_file (fname)
+            if not module:
                 continue
-            info = imp.find_module (mname, [self.application.translators_path])
-            print imp.load_module (mname, *info)
+
+    def load_translator_from_file (self, fname):
+        # 1. Split the path and get the latest part of it
+        # 2. Split the extension and get the name without .py
+        mname = os.path.splitext(os.path.split(fname)[1])[0]
+        return self.load_translator (mname)
+
+    def load_translator (self, mname):
+        if mname == '__init__':
+            return
+        info = imp.find_module (mname, [self.application.translators_path])
+        print imp.load_module (mname, *info)
 
