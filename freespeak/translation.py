d963e192b7ca114b66dc58b42ae6ea6178e99559
lethalman <lethalman@aecf430d-c2fa-0310-b08f-8e4b508fadd5>
2008-10-11 13:01:09 +0000

    Fill Translation class
    git-svn-id: svn+ssh://svn.gna.org/svn/freespeak/trunk@51 aecf430d-c2fa-0310-b08f-8e4b508fadd5

--- freespeak/translation.py
+++ freespeak/translation.py
@@ -1,6 +1,42 @@
-class Translation (object):
+import threading
+
+class Translation (threading.Thread):
+    STATUS_STARTED = 0
+    STATUS_PROGRESS = 1
+    STATUS_COMPLETE = 2
+
     def __init__ (self, application):
         self.application = application
+        self.translator = None
+        self.status = None
+        self.status_text = None
+
+    def set_translator (self, translator):
+        self.translator = translator
+        self.language_table = self.translator.get_language_table ()
+        self.update_from_langs (self.language_table.keys ())
+
+    def set_from_lang (self, lang):
+        self.from_lang = lang
+        self.update_to_langs (self.language_table[self.from_lang])
+
+    def set_to_lang (self, lang):
+        self.to_lang = lang
+
+    def update_from_langs (self, langs):
+        raise NotImplementedError ()
+
+    def update_to_langs (self, langs):
+        raise NotImplementedError ()
+
+    def update_status (self, status, status_text):
+        raise NotImplementedError ()
+
+    def run (self, data):
+        self.update_status (Translation.STATUS_STARTED, _("Translation started"))
+        for status in self.translator.translate (self.from_lang, self.to_lang, data):
+            self.update_status (Translation.STATUS_PROGRESS, status)
+        self.update_status (Translation.STATUS_COMPLETE, _("Translation complete"))
 
 class TextTranslation (Translation):
     pass
