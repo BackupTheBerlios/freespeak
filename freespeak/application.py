17517d0819bf24beef3aee91e656e859f9452de2
lethalman <lethalman@aecf430d-c2fa-0310-b08f-8e4b508fadd5>
2008-10-11 12:57:49 +0000

    Started refactoring
    git-svn-id: svn+ssh://svn.gna.org/svn/freespeak/trunk@26 aecf430d-c2fa-0310-b08f-8e4b508fadd5

--- freespeak/application.py
+++ freespeak/application.py
@@ -1,8 +1,8 @@
 """
-    freespeak.py
-    Fri Jun 14 13:41:56 2004
+FreeSpeak - a simple frontend to already existing online translators
+application.py
+
     Copyright (C) 2005-2006-2007-2008  Luca Bruno <lethalman88@gmail.com>
-    http://www.italianpug.org
    
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,37 +19,74 @@
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 """
 
-if __name__ == '__main__': 
-    import os, sys, os.path, tempfile
-    # I use C-c C-c in GNU Emacs, and the argv[0] is blank
-    try:
-        os.chdir(os.path.dirname(sys.argv[0]))
-    except: pass
+__author__ = "Luca Bruno <lethalman88@gmail.com>"
+__version__ = "0.1.1"
+__homepage__ = "http://home.gna.org/freespeak"
 
-    from src.nls import Locale
-    from src.config import Config
+import gettext
+import tempfile
+import sys
 
-    locale = Locale()
-    config = Config()
-    locale.use(config.get("interface", "language"))
+import gtk
+from gtk import gdk
 
-    from optparse import OptionParser
+from freespeak.config import Config
+from freespeak.ui.main_window import MainWindow
+from freespeak.ui.exception_dialog import ExceptionDialog
+#from freespeak.ipc import IpcServer, IpcClient
 
-    parser = OptionParser(usage='%prog [-c] [-q] [ module [ from [ to ] ] ]',
-                          version='FreeSpeak 0.1.1') 
-    parser.add_option('-c', '--clipboard',
-            action='store_true', dest='clipboard', default=False,
-            help=_('use clipboard instead of stdin'))
-    parser.add_option("-q", "--quiet",
-            action="store_false",dest="show_window", default=True,
-            help=_("start minimized in tray icon"))
-    
-    options, args = parser.parse_args()
-    PID = os.path.join(tempfile.gettempdir(),
-                       'freespeak'+str(os.getuid())+'.pid')
-    SOCK = os.path.join(tempfile.gettempdir(),
-                        'freespeak'+str(os.getuid())+'.ipc')
-    
+class Application (object):
+    def __init__ (self):
+        self.domain = 'freespeak'
+        self.setup_l10n ()
+        self.setup_exception_dialog ()
+        self.setup_config ()
+        self.setup_cmdline ()
+        self.setup_ipc ()
+
+    def setup_exception_dialog ():
+        sys.excepthook = ExceptionDialog
+
+    def setup_l10n (self):
+        gettext.NullTranslations()
+        gettext.install(self.domain)
+
+    def setup_config (self):
+        self.config = Config ()
+
+    def setup_cmdline (self):
+        parser = OptionParser(usage='%prog [-c] [-q] [ module [ from [ to ] ] ]',
+                              version='FreeSpeak %s' % __version__) 
+        parser.add_option('-c', '--clipboard',
+                          action='store_true', dest='clipboard', default=False,
+                          help=_('use clipboard instead of stdin'))
+        parser.add_option("-q", "--quiet",
+                          action="store_false",dest="show_window", default=True,
+                          help=_("start minimized in tray icon"))
+        
+        self.options, self.args = parser.parse_args()
+
+    def setup_ipc (self):
+        self.pid_file = os.path.join(tempfile.gettempdir(), 'freespeak'+str(os.getuid())+'.pid')
+        self.sock_file = os.path.join(tempfile.gettempdir(), 'freespeak'+str(os.getuid())+'.sock')
+        self.ipc_server
+
+    def start (self):
+        #client = IpcClient (self)
+        #try:
+        #    client.execute_start ()
+        #    return
+        #except:
+        #    self.ipc_server = IpcServer (self)
+
+        self.main_window = MainWindow ()
+        self.main_window.show ()
+        gtk.main ()
+
+if __name__ == '__main__':
+    app = Application ()
+    app.start ()
+
     import gtk, gtk.gdk
     from src.settings import Settings
     from src.translation import Translation, MiniTranslation
---
+++ freespeak/ui/exception_dialog.py
@@ -0,0 +1,25 @@
+import traceback
+
+class ExceptionDialog (gtk.Dialog):
+    """
+    Show non-catched exception in a window
+    """
+    def __init__(self, *tb):
+        gtk.Dialog.__init__(self, 'FreeSpeak - '+_('Exception'), None, 0,
+                            (gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
+        self.set_keep_above (1)
+        self.set_resizable (1)
+        self.set_default_size (400, 300)
+        
+        label = gtk.Label()
+        label.set_markup('<span color="red"><b>'+
+                         _('An error was occurred:')+'</b></span>')
+        label.show()
+        self.vbox.pack_start(label, 0, 0)
+        text = gtk.TextView()
+        text.get_buffer().set_text(''.join(traceback.format_exception (*tb)))
+        scroll = make_text (text)
+        scroll.show()
+        self.vbox.pack_start(scroll)
+        
+        self.run ()
