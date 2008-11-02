771d1ec722ca6e29c9f81ae07b7b52bf3dc354a7
lethalman <lethalman@aecf430d-c2fa-0310-b08f-8e4b508fadd5>
2008-11-02 22:56:05 +0000

    Forgot to add ui/about.py
    git-svn-id: svn+ssh://svn.gna.org/svn/freespeak/trunk@116 aecf430d-c2fa-0310-b08f-8e4b508fadd5

---
+++ freespeak/ui/about.py
@@ -0,0 +1,65 @@
+#     about.py
+#     Sat Jul 16 14:44:37 2005
+#     Copyright (C) 2005-2006-2007-2008  Luca Bruno <lethalman88@gmail.com>
+#     http://www.italianpug.org
+   
+#     This program is free software; you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation; either version 2 of the License, or
+#     (at your option) any later version.
+    
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Library General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+import gtk
+import gnome
+
+class About (gtk.AboutDialog):
+    def __init__ (self, application):
+        gtk.AboutDialog.__init__ (self)
+        self.application = application
+
+        self.set_name ("FreeSpeak")
+        self.set_version (application.version)
+        self.set_comments (_("A free frontend to online translator engines")) 
+        self.set_license ("""
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 2 of the License, or
+        (at your option) any later version.
+        
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Library General Public License for more details.
+        
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+        """)
+        gtk.about_dialog_set_url_hook (self.on_url, "")
+        gtk.about_dialog_set_email_hook (self.on_url, "mailto:")
+                          
+        self.set_website_label ("http://home.gna.org/freespeak/")
+        self.set_website ("http://home.gna.org/freespeak/")
+        self.set_authors (["Luca Bruno\t<lethalman88@gmail.com>"])
+        self.set_logo (self.application.icon_theme.load_icon ('freespeak', 64, 0))
+        self.set_artists (["Coviello Giuseppe\t<immigrant@email.it>"])
+        self.set_translator_credits ("Luca Bruno\t<lethalman88@gmail.com>")
+                                     
+        self.set_copyright ("Copyright (C) 2005-2006-2007-2008 Luca Bruno <lethalman88@gmail.com>")
+
+        self.connect ('response', self.on_response)
+        self.show_all()
+
+    def on_url (self, w, url, data):
+        gnome.url_show (data+url)
+
+    def on_response (self, *w):
+        self.destroy ()
