a05324912f2982d0a7739fdfa07cd43fe38ff71c
lethalman <lethalman@aecf430d-c2fa-0310-b08f-8e4b508fadd5>
2008-11-02 22:53:34 +0000

    Added it translation. Copyright headers as comments
    git-svn-id: svn+ssh://svn.gna.org/svn/freespeak/trunk@110 aecf430d-c2fa-0310-b08f-8e4b508fadd5

--- freespeak/ui/settings.py
+++ freespeak/ui/settings.py
@@ -1,23 +1,21 @@
-"""
-    settings.py
-    Fri Jun 14 13:41:56 2004
-    Copyright (C) 2005-2006-2007-2008  Luca Bruno <lethalman88@gmail.com>
-    http://www.italianpug.org
+#     settings.py
+#     Fri Jun 14 13:41:56 2004
+#     Copyright (C) 2005-2006-2007-2008  Luca Bruno <lethalman88@gmail.com>
+#     http://www.italianpug.org
    
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+#     This program is free software; you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation; either version 2 of the License, or
+#     (at your option) any later version.
     
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Library General Public License for more details.
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Library General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-"""
+#     You should have received a copy of the GNU General Public License
+#     along with this program; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 import gtk
 from freespeak import utils
@@ -27,9 +25,6 @@ from freespeak.ui.translation_box import TranslatorCombo
 class Settings(gtk.Dialog):
     @utils.syncronized
     def __init__(self, application):
-        """
-        FreeSpeak user preferences
-        """
         gtk.Dialog.__init__ (self, _('Preferences'), application.main_window, 0,
                              (gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
         self.application = application
--- freespeak/ui/utils.py
+++ freespeak/ui/utils.py
@@ -1,3 +1,23 @@
+# FreeSpeak - a simple frontend to already existing online translators
+# ui/utils.py
+
+#     Sun Jun 26 16:42:52 2005
+#     Copyright (C) 2005-2006-2007-2008  Luca Bruno <lethalman88@gmail.com>
+   
+#     This program is free software; you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation; either version 2 of the License, or
+#     (at your option) any later version.
+    
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Library General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
 import gtk
 import gobject
 
@@ -47,9 +67,6 @@ class Progress (gtk.ProgressBar):
         self.running = False
 
 def error (message, parent=None):
-    """
-    Run an message dialog for an error
-    """
     dialog = gtk.MessageDialog(parent,
                                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                                gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, message)
@@ -57,9 +74,6 @@ def error (message, parent=None):
     dialog.destroy()
 
 def warning (message, parent=None):
-    """
-    Run an message dialog for a warning
-    """
     dialog = gtk.MessageDialog(parent,
                                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                                gtk.MESSAGE_WARNING, gtk.BUTTONS_OK, message)
@@ -67,12 +81,6 @@ def warning (message, parent=None):
     dialog.destroy()
             
 def question(self, message, parent=None):
-    """
-    Ask a question and return the response of the message dialog
-    @param msg: The message to be shown
-    @param parent: Specify the transient parent
-    @return: Dialog response
-    """
     dialog = gtk.MessageDialog(parent,
                                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                                gtk.MESSAGE_QUESTION, gtk.BUTTONS_NONE, msg)
