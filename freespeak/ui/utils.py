f85416f8a8dd6a71d77d35f5f3fdf96b1686ae11
Luca Bruno <lethalman88@gmail.com>
2009-01-07 14:52:34 +0100

    Pychecker/Pylink/Pyflakes clean (freespeak.ui.utils). 	* freespeak/ui/utils.py: added docstrings 	Cut lines to 80 characters. 	(MessageDialog.__init__): shadowed built-in 'type'

--- freespeak/ui/utils.py
+++ freespeak/ui/utils.py
@@ -19,10 +19,17 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
+"""
+GUI utilities
+"""
+
 import gtk
 import gobject
 
 class ScrolledWindow (gtk.ScrolledWindow):
+    """
+    Create a scrolled window with particular options
+    """
     def __init__ (self, child):
         gtk.ScrolledWindow.__init__ (self)
         self.set_policy (gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
@@ -30,6 +37,9 @@ class ScrolledWindow (gtk.ScrolledWindow):
         self.add (child)
 
 class Frame (gtk.Frame):
+    """
+    HIG compliant frame
+    """
     def __init__ (self, label_text, extra_widget=None):
         gtk.Frame.__init__ (self)
         widget = label = gtk.Label ()
@@ -45,6 +55,9 @@ class Frame (gtk.Frame):
         self.set_shadow_type (gtk.SHADOW_NONE)
 
     def add (self, widget):
+        """
+        Overridden container method to add the widget respecting the HIG
+        """
         alignment = gtk.Alignment ()
         alignment.set_property ('top-padding', 6)
         alignment.set_property ('left-padding', 12)
@@ -55,6 +68,10 @@ class Frame (gtk.Frame):
         return gtk.Frame.add (self, alignment)
 
 class Progress (gtk.HBox):
+    """
+    A progress bar to be used as a user feedback.
+    The widget contains a cancellation button.
+    """
     __gsignals__ = {
         'cancelled': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
         }
@@ -67,43 +84,70 @@ class Progress (gtk.HBox):
         self.setup_cancel ()
 
     def setup_bar (self):
+        """
+        Setup the progress bar
+        """
         self.bar = gtk.ProgressBar ()
         self.bar.set_pulse_step (0.01)
         self.bar.show ()
         self.pack_start (self.bar)
 
     def setup_cancel (self):
+        """
+        Setup the cancellation button
+        """
         self.cancel = TinyButton (gtk.STOCK_CANCEL)
         self.cancel.connect ('clicked', self.on_cancel)
         self.cancel.show ()
         self.pack_start (self.cancel, False)
 
     def pulse_idle (self):
+        """
+        GObject idle for pulsing the progress bar
+        """
         self.bar.pulse ()
         return self.running
 
     # API
 
     def is_running (self):
+        """
+        Returns True if the progress is pulsing, False otherwise
+        """
         return self.running
 
     def set_text (self, text):
+        """
+        Set the text inside the progress bar
+        """
         self.bar.set_text (text)
 
     def start (self):
+        """
+        Start pulsing the progress bar
+        """
         assert not self.running
         self.running = True
         gobject.timeout_add (10, self.pulse_idle)
 
     def stop (self):
+        """
+        Stop the progress
+        """
         self.running = False
 
     # Events
 
     def on_cancel (self, button):
+        """
+        Emit the 'cancelled' signal the the cancellation button is clicked
+        """
         self.emit ('cancelled')
 
 class TinyButton (gtk.Button):
+    """
+    A small tiny and eye-candy button to be used beside widgets
+    """
     def __init__ (self, stock=None, size=gtk.ICON_SIZE_MENU):
         gtk.Button.__init__ (self)
         self.set_name ("tiny-button")
@@ -114,18 +158,32 @@ class TinyButton (gtk.Button):
             self.set_image (image)
 
 class MessageDialog (gtk.MessageDialog):
-    def __init__ (self, parent, message, type, buttons):
-        gtk.MessageDialog.__init__ (self, parent, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                                    type, buttons, message)
+    """
+    Convenient class for creating message dialogs
+    """
+    def __init__ (self, parent, message, mtype, buttons):
+        flags = gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT
+        gtk.MessageDialog.__init__ (self, parent, flags, mtype, buttons, message)
         self.connect ('response', self.on_response)
 
     def on_response (self, dialog, response):
+        """
+        Destory the dialog on whatever response
+        """
         dialog.destroy ()
 
 def error (message, parent=None):
-    dialog = MessageDialog (parent, message, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK)
+    """
+    Show an error message dialog
+    """
+    dialog = MessageDialog (parent, message,
+                            gtk.MESSAGE_ERROR, gtk.BUTTONS_OK)
     dialog.show_all ()
 
 def warning (message, parent=None):
-    dialog = MessageDialog (parent, message, gtk.MESSAGE_WARNING, gtk.BUTTONS_OK)
+    """
+    Show a warning message dialog
+    """
+    dialog = MessageDialog (parent, message,
+                            gtk.MESSAGE_WARNING, gtk.BUTTONS_OK)
     dialog.show_all ()
