028f7ff642456c0db41e9ee7144cf5f2b9156115
Luca Bruno <lethalman88@gmail.com>
2009-01-07 12:16:43 +0100

    Pychecker/Pylink/Pyflakes clean (freespeak.ui.manager). 	* freespeak/ui/manager.py: added docstrings

--- freespeak/ui/manager.py
+++ freespeak/ui/manager.py
@@ -19,27 +19,46 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
+"""
+Translation manager UI
+"""
+
 import gtk
 
 class Manager (gtk.Notebook):
+    """
+    A notebook for managing translation pages
+    """
     def __init__ (self, application):
         gtk.Notebook.__init__ (self)
         self.set_scrollable (True)
         self.application = application
 
     def switch_to_latest (self):
+        """
+        Switch to the latest page of the notebook
+        """
         self.set_current_page (-1)
 
     def close_current_translation (self):
+        """
+        Close the currently viewed translation
+        """
         page_num = self.get_current_page ()
         self.remove_page (page_num)
 
     def add_translation (self, translation):
+        """
+        Append a new translation page
+        """
         label = translation.get_label ()
         self.append_page (translation, label)
         translation.show ()
 
     def remove_translation (self, translation):
+        """
+        Remove the given translation from the notebook
+        """
         page_num = self.page_num (translation)
         self.remove_page (page_num)
 
